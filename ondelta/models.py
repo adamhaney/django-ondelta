e8ca4fce3ca35d3e66917a7c47ab168cf337e472
Adam Haney <adam.haney@akimbo.io>
2013-10-26 19:33:48 -0400

    * Converted over to a scheme that doesn't require keeping a revision history. This makes the project have a much smaller number of concerns and has the bonus of not hitting the database. I basically started over with a more TDD approach to this code base and think that this is a much easier approach

--- ondelta/models.py
+++ ondelta/models.py
@@ -6,104 +6,58 @@ from django.utils.timezone import now
 
 
 class OnDeltaMixin(models.Model):
-    _delta_notification_history = models.TextField(default='', null=True, editable=False)
 
     class Meta:
         abstract = True
 
-    def _get_history(self):
-        try:
-            return pickle.loads(self._delta_notification_history)
-        except:
-            return []
+    def __init__(self, *args, **kwargs):
+        super_return = super(OnDeltaMixin, self).__init__(*args, **kwargs)
+        self._ondelta_snapshot_state()
+        return super_return
 
-    def _get_current_time(self):
+    def _ondelta_snapshot_state(self):
+        self.model_snapshot = copy.deepcopy(self)
+
+    def _ondelta_fields_to_watch(self):
         """
-        This method exists to make test stubbing easier
+        This gives us all the fields that we should care about changes
+        for, excludes fields added by tests (nose adds 'c') and the id
+        which is an implementation detail of django
         """
-        return now()
+        return set(self._meta.get_all_field_names()) - set(['c', 'id'])
 
-    def __create_change_record(self):
-        model_copy = copy.deepcopy(self)
-        del model_copy._delta_notification_history
-
-        return {
-            'modified_time': self._get_current_time(),
-            'model': model_copy
-        }
-
-    def __add_change_record(self):
-        """
-        Add a change record to the model's history
-        """
-
-        history = self._get_history()
-        history.append(self.__create_change_record())
-        self._delta_notification_history = pickle.dumps(history)
-
-    def _delta_fields_to_watch(self):
-        return set(self._meta.get_all_field_names()) - set(['id', 'c', '_delta_notification_history'])
-
-    def _delta_get_old_field_value(self, field):
-        """
-        Get the most recent previous value for a given field
-        """
-        return getattr(self._get_history().pop()['model'], field)
-
-    def _delta_get_new_field_value(self, field):
-        """
-        Get the current value of the field
-        """
-        return getattr(self, field)
-
-    def _delta_ne_fields(self, field, old_value, new_value):
-        """
-        Notifications are sent that a field changed when two fields
-        that are compared to one another evalue to be unequal. This
-        method evaluates whether or not two fields are equal, you may
-        wish to override this depending upon your application's view
-        of inequality.
-        """
-        return old_value != new_value
-
-    def _delta_call_ondelta_methods(self, field, old_value, new_value):
-        getattr(self, "ondelta_{}".format(field))(old_value, new_value)
-
-    def _delta_check_changes_and_notify(self):
-        fields_changed = False
-
-        for field in self._delta_fields_to_watch():
-            old_value = self._delta_get_old_field_value(field)
-            new_value = self._delta_get_new_field_value(field)
-            if self._delta_compare_fields(field, old_value, new_value):
-                # If any fields have changed save a new
-                # revision (no need to duplicate the same data
-                # over and over again)
-                fields_changed = True
-                self._delta_call_ondelta_methods(field, old_value, new_value)
-
+    def _ondelta_get_differences(self):
+        fields_changed = {}
+        print 
+        for field in self._ondelta_fields_to_watch():
+            snapshot_value = getattr(self.model_snapshot, field)
+            current_value = getattr(self, field)
+            if snapshot_value != current_value:
+                fields_changed[field] = {
+                    'old': snapshot_value,
+                    'new': current_value
+                }
         return fields_changed
 
+    def _ondelta_dispatch_notifications(self):
+        fields_changed = self._ondelta_get_differences()
+        for field, changes in fields_changed.items():
+            method_name = "ondelta_{}".format(field)
+            print method_name
+            if hasattr(self, method_name):
+                getattr(self, method_name)(changes['old'], changes['new'])
+
+        if fields_changed.keys():
+            self.ondelta_all(fields_changed=fields_changed)
+
+    def ondelta_all(self, fields_changed):
+        """
+        Child classes interested in executing logic based upon
+        aggregate field changes should override this method
+        """
+        pass
+        
     def save(self, *args, **kwargs):
-        # We don't always want to save a revision if the model hasn't
-        # changed, so start from the default position that a revision
-        # should not be created
-        save_revision = False
-
-        # If we don't have any history this is a new model, so we need
-        # to save an initial copy to diff against
-        if len(self._get_history()) == 0:
-            save_revision = True
-
-        # If we do have history, diff against it and see if anything has changed
-        else:
-            save_revision = self._delta_check_changes_and_notify()
-
-        # Always save *after* we've check for changes
-        if save_revision:
-            self.__add_change_record()
-
-        # Call super on save so we persist
-        # NOTE: this means if the parent save fails then we could
-        # accidentally send multiple notifications
+        self._ondelta_dispatch_notifications()
+        self._ondelta_snapshot_state()
         return super(OnDeltaMixin, self).save(*args, **kwargs)
