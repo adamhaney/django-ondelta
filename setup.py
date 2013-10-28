9cf7d748834fe62379f650d61cefdf33838726a9
Adam Haney <adam.haney@akimbo.io>
2013-10-28 16:19:32 -0400

    * bumping version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(fname):
 
 setup(
     name="ondelta",
-    version="0.1.5",
+    version="0.2.0",
     author="Adam Haney",
     author_email="adam.haney@campusbellhops.com",
     description=DESCRIPTION,
